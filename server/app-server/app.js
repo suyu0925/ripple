c4ca5fed8a525b90e868fd963edc2fcc9e492961
lckof-mac <suyu0925@gmail.com>
2015-02-28 22:55:35 +0800

    temp commit.

--- server/app-server/app.js
+++ server/app-server/app.js
@@ -1,28 +1,9 @@
-var http = require('http');
-var url = require('url');
-var port = 2345;
+var express = require('express');
+var app = express();
+var port = 3000;
 
-function onRequest(req, res) {
-	var pathname = url.parse(req.url, true).pathname;
-	var arg = url.parse(req.url, true).query;
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
 
-	console.log('pathname: ' + pathname);
-	console.log('arg: ' + JSON.stringify(arg));
-
-	if (pathname == '/login') {
-		if (arg.device_id) {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end('device_id = ' + arg.device_id + '\n');			
-		} else {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end('Login: no device_id\n');
-		}
-	} else {
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello World\n');
-	}
-}
-
-http.createServer(onRequest).listen(port);
-
-console.log('app server is running on port: ' + port);
+app.listen(port);
